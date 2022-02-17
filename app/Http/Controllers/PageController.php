9eb3c656532f73528c9dac5aadcc9c60c6725331
Stefano Ruggiu <steruggiu@gmail.com>
2022-02-17 16:09:56 +0000

    non mi si connetta al db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
+
+
+
 
 class PageController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $movies = Movie::all();
+        dd($movies);
         return view('home');
     }
 }
+
+
+// 7 All’interno della funzione index() del controller, recuperate tutti i film dal database e passateli alla view, che quindi li visualizzerà a schermo, tramite delle card.
